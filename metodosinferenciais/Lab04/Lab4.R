c462907ba2e7e0d05a1419545c9b7f9e93b7688b
Fpschwartz1 <fpschwartz@gmail.com>
2021-04-28 23:35:40 -0300

    Teste de Hipótese na prática

--- metodosinferenciais/Lab04/Lab4.R
+++ metodosinferenciais/Lab04/Lab4.R
@@ -1,52 +1,100 @@
-rm(list = ls(all = TRUE)) # apaga variaveis no Global Environment
-source("NormalCompara.R") # carrega a funcao dnormalCopm()
+rm(list = ls(all = TRUE))
 
-# exercicio 1
-# http://www.portalaction.com.br/en/node/1948
-a1 <- c(
-      18.8,17.591,20.835,19.169,18.755,20.504,
-      18.756,17.527,19.29,19.203,18.621,18.977,17.078,22.059,18.419,
-      19.919,20.308,17.62,18.585,20.764,21.117,18.899,21.426,17.89,21.055
-)
+source("NormalCompara.R")
 
-a2 <- c(
-      22.284,22.057,22.629,24.62,21.491,21.198,
-      21.901,22.881,22.86,22.058,22.699,22.909,
-      25.302,17.968,24.515,23.15,24.662,23.327,
-      22.447,23.382,22.426,22.787,21.983,24.534,
-      22.771,21.043,21.203,24.009,21.917,21.152
-)
+######################################
+# Teste de hipotese para uma amostra
+######################################
 
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc=">")
+# verifique se as amostras abaixo pertencem aa populacao
+# cujo status quo eh ve
 
-# exercicio 2
-# gera arquivo csv
-# set.seed(837364)
-# df <- data.frame(
-#          a1 = rnorm(100,mean=45,sd=10),
-#          a2 = rnorm(100,mean=48,sd=8),
-#          a3 = rnorm(100,mean=47,sd=2)
-#)
-#write.csv(df,file="exercicio.csv",row.names = FALSE)
+ve <- 50
 
-df <- read.csv("exercicio.csv",sep=",", header=TRUE)
-
-a1 <- df$a1
-a2 <- df$a2
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc="=")
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc=">",nc=.95) # nc 99%
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc="<")
+# 1
+set.seed(764939)
+a1 <- rnorm(30, 30, 20)
+dnormalComp(ve, NULL, mean(a1), sd(a1)/sqrt(length(a1)))
 
 
-a1 <- df$a2
-a2 <- df$a3
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc="=")
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc=">")
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc="<")
+# 2
+set.seed(764939)
+a2 <- rnorm(20, 60, 20)
+dnormalComp(ve, NULL, mean(a2), sd(a2)/sqrt(length(a2)))
+dnormalComp(ve,NULL,mean(a2),sd(a2)/sqrt(length(a2)), rc="<",
+            nc=.9)
+
+# 3
+set.seed(764939)
+a3 <- rnorm(60, 60, 20)
+dnormalComp(ve, NULL, mean(a3), sd(a3)/sqrt(length(a3)), rc="<")
+dnormalComp(ve, NULL, mean(a3), sd(a3)/sqrt(length(a3)), nc=.99)
 
 
-a1 <- df$a1
-a2 <- df$a3
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc="=") # for?a do teste
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc=">")
-dnormalComp(media1=mean(a1),dp1=sd(a1)/sqrt(length(a1)),media2=mean(a2),dp2=sd(a2)/sqrt(length(a2)),rc="<")
+# 4
+set.seed(764939)
+a4 <- rnorm(100, 40, 110)
+dnormalComp(ve,NULL,mean(a4),sd(a4)/sqrt(length(a4)), rc=">")
+
+
+
+######################################
+# Teste de hipotese para duas amostras
+######################################
+
+# verifique se as amostras abaixo pertencem aa mesma populacao
+
+# 1
+set.seed(764939)
+a1 <- rnorm(25,19,2.5)
+a2 <- rnorm(30,22,2)
+
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)))
+
+# 2
+set.seed(764939)
+a1 <- rnorm(25,20,2.5)
+a2 <- rnorm(30,22,2)
+
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)))
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)), rc=">")
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)), rc="<")
+
+# 3
+set.seed(764939)
+a1 <- rnorm(30,21,2.7)
+a2 <- rnorm(80,22,5)
+
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)))
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)), rc=">")
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)), rc="<")
+
+
+# 4 - forca do teste
+set.seed(764939)
+a1 <- rnorm(50,6,3)
+a2 <- rnorm(30,8,4)
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)))
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)), ft=1)
+
+dnormalComp(mean(a1), sd(a1)/sqrt(length(a1)), 
+            mean(a2), sd(a2)/sqrt(length(a2)), ft=2)
+
--- metodosinferenciais/Lab04/NormalCompara.R
+++ metodosinferenciais/Lab04/NormalCompara.R
@@ -1,74 +1,108 @@
-dnormalComp <- function(media1=0, dp1=1, media2=0, dp2=1, nc=.95, rc="=")
-{
 
-  ########################
-  # Script principal
-  ########################
+dnormalComp <- function(media1=0, ep1=1, media2=0, ep2=1, nc=.95, rc="=", ft=NULL)
+{
+  # cores das curvas 
+  cores=c("red", "blue")
+  
+  # verificacao dos parametros
+  if(media1 > media2){
+    # troca medias
+    aux <- media1; media1 <- media2; media2 <- aux
+    # troca erros padrao
+    aux <- ep1; ep1 <- ep2; ep2 <- aux
+    
+    if(!is.null(ft)){
+      if(ft==1) { 
+        ft <- 2
+      } else ft <- 1
+    }
+  }
+
+  if(is.null(ep1) & is.null(ep2)) return("Erros padrao nulos simultaneamente")
+  ep1_i <- ep1; ep2_i <- ep2
+    
+  # verifica se o teste eh de uma ou duas amostras
+  if(is.null(ep1) | is.null(ep2)) {
+    titulo <- "Teste de hipotese para uma amostra"
+    ft <- NULL # nao calcula forca do teste
+  
+    if(is.null(ep1)) ep1_i <- 1
+    if(is.null(ep2)) ep2_i <- 1
+    
+  } else titulo <- "Teste de hipotese para duas amostras"
   
   # eixo x da curva normal
   lim <- c(
-           min(c(media1+c(-4,4)*dp1, media2+c(-4,4)*dp2)), 
-           max(c(media1+c(-4,4)*dp1, media2+c(-4,4)*dp2))
-         )
+    min(c(media1+c(-4,4)*ep1_i, media2+c(-4,4)*ep2_i)), 
+    max(c(media1+c(-4,4)*ep1_i, media2+c(-4,4)*ep2_i))
+  )
+
   x <- seq(lim[1], lim[2], by = 0.01)
   
   # curva normal
-  cn1 <- function(x) {dnorm(x,media1,dp1)} # curva normal
-  cn2 <- function(x) {dnorm(x,media2,dp2)} # curva normal
+  cn1 <- function(x) {dnorm(x,media1,ep1_i)} # curva normal 1
+  cn2 <- function(x) {dnorm(x,media2,ep2_i)} # curva normal 2
   
-  # traça as curvas normais 1 e 2
-  if(cn1(media1)>=cn2(media2)){
+  # traca as curvas normais 1 e 2
+  if((cn1(media1)>=cn2(media2) & !is.null(ep1)) | is.null(ep2)){
     plot(x,cn1(x),ylab="Densidade",xlab="x",
-         main="Curva Normal",type="l",lwd=2)
-    lines(x,cn2(x),lwd=2, col="red")
+         main=titulo,type="l",lwd=1.5, col=cores[1])
   } else {
     plot(x,cn2(x),ylab="Densidade",xlab="x",
-         main="Curva Normal",type="l",lwd=2,col="red")
-    lines(x,cn1(x),lwd=2)
+         main=titulo,type="l",lwd=1.5, col=cores[2])
+  }
+ 
+  # intervalos de confianca
+  if(rc=="="){
+    xI11 <- media1 - qnorm(nc+(1-nc)/2)*ep1_i
+    xI12 <- media1 + qnorm(nc+(1-nc)/2)*ep1_i
+    xI21 <- media2 - qnorm(nc+(1-nc)/2)*ep2_i
+    xI22 <- media2 + qnorm(nc+(1-nc)/2)*ep2_i
+  } else if(rc=="<"){
+    xI21 <- media2 + qnorm(1-nc)*ep2_i
+    xI22 <- lim[2]
+  } else if(rc==">"){
+    xI11 <- lim[1]
+    xI12 <- media1 + qnorm(nc)*ep1_i
   }
     
+  if((rc %in% c("=",">")) & !is.null(ep1)){
+    ic <- seq(xI11, xI12, by = 0.01)
+    polygon(c(min(ic), ic, max(ic)), c(0, cn1(ic),0), col=scales::alpha(col=cores[1],.4), border = cores[1])
+  }
+  
+  if((rc %in% c("=","<")) & !is.null(ep2)){
+    ic <- seq(xI21, xI22, by = 0.01)
+    polygon(c(min(ic), ic, max(ic)), c(0, cn2(ic),0), col=scales::alpha(col=cores[2],.4), border = cores[2])
+  }
+  
+  # linhas da media
+  lines(c(media1,media1),c(-1,cn1(media1)),lwd=1.5, col = cores[1])
+  lines(c(media2,media2),c(-1,cn2(media2)),lwd=1.5, col = cores[2])
 
-  # linha horzontal em zero
+  # linha das curvas
+  if(!is.null(ep2)) lines(x,cn2(x),lwd=2, col = cores[2])
+  if(!is.null(ep1)) lines(x,cn1(x),lwd=2, col = cores[1])
+  
+  # linha horizontal em zero
   lines(lim,c(0,0))
   
-  # linhas da média
-  lines(c(media1,media1),c(-1,cn1(media1)),lwd=4,type="l")
-  lines(c(media2,media2),c(-1,cn2(media2)),lwd=4,type="l",col="red")
-  
-  # intervalos de confiaça
-  if(rc=="="){
-    xI11 <- media1 - qnorm(nc+(1-nc)/2)*dp1
-    xI12 <- media1 + qnorm(nc+(1-nc)/2)*dp1
-    xI21 <- media2 - qnorm(nc+(1-nc)/2)*dp2
-    xI22 <- media2 + qnorm(nc+(1-nc)/2)*dp2
-  } else if(rc=="<"){
-    xI11 <- media1 - 4*dp1
-    xI12 <- media1 + qnorm(1-nc)*dp1
-    xI21 <- media2 - 4*dp2
-    xI22 <- media2 + qnorm(1-nc)*dp2    
-  } else if(rc==">"){
-    xI11 <- media1 + qnorm(nc)*dp1
-    xI12 <- media1 + 4*dp1
-    xI21 <- media2 + qnorm(nc)*dp2
-    xI22 <- media2 + 4*dp2    
-  }
+  # forca do teste
+  if(!is.null(ft)){
+    integral <- function(f,a,b) {i<-integrate(f,a,b); as.numeric(i[1])}
     
-  
-  inc <- (xI12-xI11)/20
-  i<-xI11+inc
-  lines(c(i,i),c(-1,cn1(i)),col="black",lty=4,lwd=2)
-  while(i < xI12){
-    lines(c(i,i),c(0,cn1(i)),col="black",lwd=0.5)
-    i<-i+inc
+    if(ft == 1){
+      xaux <- seq(xI21, xI22, by = 0.01)
+      polygon(c(min(xaux), xaux, max(xaux)), c(0, cn1(xaux),0), col="orange", border="orange")
+      beta <- integral(cn1,xI21,xI22)
+    }
+    if(ft == 2){
+      xaux <- seq(xI11, xI12, by = 0.01)
+      polygon(c(min(xaux), xaux, max(xaux)), c(0, cn2(xaux),0), col="orange", border="orange")
+      beta <- integral(cn2,xI11,xI12)
+    }
+    
+    return(paste0("Forca do teste: ", 1-beta, "  beta: ", beta))
   }
-  lines(c(i,i),c(-1,cn1(i)),col="black",lty=4,lwd=2)
   
-  inc <- (xI22-xI21)/20
-  i<-xI21+inc
-  lines(c(i,i),c(-1,cn2(i)),col="red",lty=4,lwd=2)
-  while(i < xI22){
-    lines(c(i,i),c(0,cn2(i)),col="red",lwd=0.5)
-    i<-i+inc
-  }
-  lines(c(i,i),c(-1,cn2(i)),col="red",lty=4,lwd=2)
-}
+}      
