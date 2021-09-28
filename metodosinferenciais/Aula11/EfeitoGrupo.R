6f3a591840a123e5e2e311340b756c6b3e5aeda8
Fpschwartz1 <fpschwartz@gmail.com>
2021-09-28 13:13:49 -0300

    Variáveis Dummies

--- metodosinferenciais/Aula11/EfeitoGrupo.R
+++ metodosinferenciais/Aula11/EfeitoGrupo.R
@@ -20,12 +20,15 @@ head(df)
 x <- df$anos
 y <- df$salario
 t <- df$escolaridade
+
 n <- nrow(df)
 
 plot(x, y, type = "n", frame = TRUE,
      main="Simulação 1", xlab = "anos", ylab = "Salario em R$ (x1000)")
-abline(h = mean(y[1 : (n/2)]), lwd = 3, col = "lightblue")
-abline(h = mean(y[(n/2 + 1) : n]), lwd = 3, col = "salmon")
+lines(c(0,0), c(-1,40), lty = "dotted" )
+abline(h = mean(y[1 : (n/2)]), lwd = 3, col = "lightblue") # nivel medio
+abline(h = mean(y[(n/2 + 1) : n]), lwd = 3, col = "salmon") # nivel superior
+
 abline(lm(y ~ x), lwd = 2)
 
 fit <- lm(y ~ x + t)
@@ -56,22 +59,26 @@ rm(list = ls(all = TRUE))
 
 df <- read.csv2("simulacao2.csv")
 
-x <- df$x
-y <- df$y
-t <- df$t
+y <- df$y # distância em quilômetros percorrida
+x <- df$x # tempo de treinamento em anos
+t <- df$t # tipo: amador - 0; profissional 1
+
 n <- nrow(df)
 
-plot(x, y, type = "n", frame = FALSE,
-     main="Silmulação 2")
+plot(x, y, type = "n", frame = FALSE, main="Silmulação 2",
+     xlab ="Tempo treinamento (anos)", ylab = "Distância (km)")
 abline(h = mean(y[1 : (n/2)]), lwd = 3, col = "lightblue")
 abline(h = mean(y[(n/2 + 1) : n]), lwd = 3, col = "salmon")
-abline(lm(y ~ x), lwd = 2)
 
-fit <- lm(y ~ x + t)
-summary(fit)
+fit1 <- lm(y ~ x)
+abline(fit1, lwd = 2)
+summary(fit1)$coef
 
-abline(coef(fit)[1], coef(fit)[2], lwd = 3, col = "lightblue")
-abline(coef(fit)[1] + coef(fit)[3], coef(fit)[2], lwd = 3, col = "salmon")
+fit2 <- lm(y ~ x + t)
+summary(fit2)$coef
+
+abline(coef(fit2)[1], coef(fit2)[2], lwd = 3, col = "lightblue")
+abline(coef(fit2)[1] + coef(fit2)[3], coef(fit2)[2], lwd = 3, col = "salmon")
 points(x[1 : (n/2)], y[1 : (n/2)], pch = 21, col = "black", bg = "lightblue", cex = 2)
 points(x[(n/2 + 1) : n], y[(n/2 + 1) : n], pch = 21, col = "black", bg = "salmon", cex = 2)
 
@@ -96,19 +103,24 @@ df <- read.csv2("simulacao3.csv")
 x <- df$x
 y <- df$y
 t <- df$t
+
 n <- nrow(df)
 
-plot(x, y, type = "n", frame = FALSE,
-     main="Simulação 3")
+plot(x, y, type = "n", frame = FALSE, main="Simulação 3", ylab="Y")
+lines(c(0,0), c(-1,4), lty = "dotted" )
+
 abline(h = mean(y[1 : (n/2)]), lwd = 3, col="lightblue")
 abline(h = mean(y[(n/2 + 1) : n]), lwd = 3, col="salmon")
-abline(lm(y ~ x), lwd = 2)
 
-fit <- lm(y ~ x + t)
-summary(fit)
+fit1 <- lm(y ~ x)
+abline(fit1, lwd = 2)
+summary(fit1)$coef
 
-abline(coef(fit)[1], coef(fit)[2], lwd = 3, col="lightblue")
-abline(coef(fit)[1] + coef(fit)[3], coef(fit)[2], lwd = 3, col="salmon")
+fit2 <- lm(y ~ x + t)
+summary(fit2)$coef
+
+abline(coef(fit2)[1], coef(fit2)[2], lwd = 3, col="lightblue")
+abline(coef(fit2)[1] + coef(fit2)[3], coef(fit2)[2], lwd = 3, col="salmon")
 points(x[1 : (n/2)], y[1 : (n/2)], pch = 21, col = "black", bg = "lightblue", cex = 2)
 points(x[(n/2 + 1) : n], y[(n/2 + 1) : n], pch = 21, col = "black", bg = "salmon", cex = 2)
 
@@ -133,13 +145,17 @@ df <- read.csv2("simulacao4.csv")
 x <- df$x
 y <- df$y
 t <- df$t
+
 n <- nrow(df)
 
-plot(x, y, type = "n", frame = FALSE,
-     main="Simulação 4")
+plot(x, y, type = "n", frame = FALSE, main="Simulação 4")
+lines(c(0,0), c(-1,40), lty = "dotted" )
 abline(h = mean(y[1 : (n/2)]), lwd = 3, col = "lightblue")
 abline(h = mean(y[(n/2 + 1) : n]), lwd = 3, col = "salmon")
-abline(lm(y ~ x), lwd = 2)
+
+fit1 <- lm(y ~ x)
+abline(fit1, lwd = 2)
+summary(fit1)$coef
 
 fit <- lm(y ~ x + t)
 abline(coef(fit)[1], coef(fit)[2], lwd = 3, col = "lightblue")
